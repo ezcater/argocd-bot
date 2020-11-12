fd46bf8eade428b1fc2990d99c1d725e0a2fc8e9
Cory Murphy <public@coryamurphy.com>
2020-11-12 09:34:34 -0500

    format file

--- test/argo.misc.test.ts
+++ test/argo.misc.test.ts
@@ -20,7 +20,7 @@ describe("argo-cd-bot", () => {
     // constants
     const argoCDToken = "token"
     const argoCDServer = "1.2.3.4"
-    
+
     beforeEach(() => {
         nock.disableNetConnect();
         probot = new Probot({
@@ -49,10 +49,10 @@ describe("argo-cd-bot", () => {
         nock.enableNetConnect();
     })
 
-    test("help comment posted on opened PR", async() => {
+    test("help comment posted on opened PR", async () => {
         nock("https://api.github.com")
             .post("/app/installations/2/access_tokens")
-            .reply(200, {token: "test"})
+            .reply(200, { token: "test" })
 
         // test constants
         const branch = "newBranch"
@@ -61,7 +61,9 @@ describe("argo-cd-bot", () => {
         const appDir = "projects/app1"
         const commentBody = "hello world from argocd bot!"
 
-        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, {"data": {"number": 1, "head": { "ref": branch}}})
+        nock("https://api.github.com")
+            .get("/repos/robotland/test/pulls")
+            .reply(200, { "data": { "number": 1, "head": { "ref": branch } } })
 
         // regex match post body should match diff produced by API
         let prCommentResponse = nock("https://api.github.com")
@@ -72,16 +74,16 @@ describe("argo-cd-bot", () => {
             .reply(200)
 
         let prOpenedPayload = JSON.parse(JSON.stringify(payloadPr1Opened))
-        await probot.receive({name: "pull_request", payload: prOpenedPayload})
+        await probot.receive({ name: "pull_request", payload: prOpenedPayload })
 
         // tells nock to evaluate if mocks were called with expected values
         prCommentResponse.done()
     })
 
-    test("help comment posted on PR", async() => {
+    test("help comment posted on PR", async () => {
         nock("https://api.github.com")
             .post("/app/installations/2/access_tokens")
-            .reply(200, {token: "test"})
+            .reply(200, { token: "test" })
 
         // test constants
         const branch = "newBranch"
@@ -89,25 +91,25 @@ describe("argo-cd-bot", () => {
         const appName = "app1"
         const appDir = "projects/app1"
 
-        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, {"data": {"number": 109, "head": { "ref": branch}}})
+        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, { "data": { "number": 109, "head": { "ref": branch } } })
 
         // regex match post body should match diff produced by API
         nock("https://api.github.com").post("/repos/robotland/test/issues/109/comments", /I'm a bot that helps with Kubernetes deployments/).reply(200)
 
         let helpPayload = JSON.parse(JSON.stringify(payloadPr1))
         helpPayload["comment"]["body"] = "argo help"
-        await probot.receive({name: "issue_comment", payload: helpPayload})
+        await probot.receive({ name: "issue_comment", payload: helpPayload })
     })
 
-   test("app rollback comment posted on PR", async() => {
+    test("app rollback comment posted on PR", async () => {
         nock("https://api.github.com")
             .post("/app/installations/2/access_tokens")
-            .reply(200, {token: "test"})
+            .reply(200, { token: "test" })
 
         const branch = "newBranch"
         const appName = "my_app"
 
-        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, {"data": {"number": 109, "head": { "ref": branch}}})
+        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, { "data": { "number": 109, "head": { "ref": branch } } })
 
         const child_process = require("child_process")
         const execStub = sandbox.stub(child_process, "exec")
@@ -118,18 +120,18 @@ describe("argo-cd-bot", () => {
 
         let payload = JSON.parse(JSON.stringify(payloadPr1))
         payload["comment"]["body"] = "argo rollback view " + appName
-        await probot.receive({name: "issue_comment", payload: payload})
+        await probot.receive({ name: "issue_comment", payload: payload })
     })
 
-    test("app info comment posted on PR", async() => {
+    test("app info comment posted on PR", async () => {
         nock("https://api.github.com")
             .post("/app/installations/2/access_tokens")
-            .reply(200, {token: "test"})
+            .reply(200, { token: "test" })
 
         const branch = "newBranch"
         const appName = "my_app"
 
-        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, {"data": {"number": 109, "head": { "ref": branch}}})
+        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, { "data": { "number": 109, "head": { "ref": branch } } })
 
         const child_process = require("child_process")
         const execStub = sandbox.stub(child_process, "exec")
@@ -140,13 +142,13 @@ describe("argo-cd-bot", () => {
 
         let syncPayload = JSON.parse(JSON.stringify(payloadPr1))
         syncPayload["comment"]["body"] = "argo info " + appName
-        await probot.receive({name: "issue_comment", payload: syncPayload})
+        await probot.receive({ name: "issue_comment", payload: syncPayload })
     })
 
-    test("app sync comment posted on PR", async() => {
+    test("app sync comment posted on PR", async () => {
         nock("https://api.github.com")
             .post("/app/installations/2/access_tokens")
-            .reply(200, {token: "test"})
+            .reply(200, { token: "test" })
 
         const branch = "newBranch"
         const appName = "my_app"
@@ -157,15 +159,15 @@ describe("argo-cd-bot", () => {
         execStub.withArgs("./src/sh/sync_current_branch.sh " + appName + " " + branch).yields(false, "sync success!")
 
         // bot should get sha for commit and post status check on PR with sync pending and sync success
-        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, {"data": {"number": 109, "head": { "ref": branch, "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e", "repo": { "id": 1296269, "node_id": "MDEwOlJlcG9zaXRvcnkxMjk2MjY5", "name": "Hello-World",  "full_name": "octocat/Hello-World", "owner": { "login": "octocat" }}}}});
+        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, { "data": { "number": 109, "head": { "ref": branch, "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e", "repo": { "id": 1296269, "node_id": "MDEwOlJlcG9zaXRvcnkxMjk2MjY5", "name": "Hello-World", "full_name": "octocat/Hello-World", "owner": { "login": "octocat" } } } } });
         // first status check posted is a pending
         nock("https://api.github.com").post("/repos/octocat/Hello-World/statuses/6dcb09b5b57875f334f61aebed695e2e4193db5e", /pending/).reply(200)
-    
+
         // this API is used to get the current branch name
-        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, {"data": {"number": 109, "head": { "ref": branch}}})
+        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, { "data": { "number": 109, "head": { "ref": branch } } })
 
         // get sha commit on PR and post a sync success status check
-        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, {"data": {"number": 109, "head": { "ref": branch, "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e", "repo": { "id": 1296269, "node_id": "MDEwOlJlcG9zaXRvcnkxMjk2MjY5", "name": "Hello-World",  "full_name": "octocat/Hello-World", "owner": { "login": "octocat" }}}}});
+        nock("https://api.github.com").get("/repos/robotland/test/pulls").reply(200, { "data": { "number": 109, "head": { "ref": branch, "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e", "repo": { "id": 1296269, "node_id": "MDEwOlJlcG9zaXRvcnkxMjk2MjY5", "name": "Hello-World", "full_name": "octocat/Hello-World", "owner": { "login": "octocat" } } } } });
         nock("https://api.github.com").post("/repos/octocat/Hello-World/statuses/6dcb09b5b57875f334f61aebed695e2e4193db5e", /success/).reply(200)
 
         // lastly, bot posts a comment on PR with sync success
@@ -174,6 +176,6 @@ describe("argo-cd-bot", () => {
 
         let syncPayload = JSON.parse(JSON.stringify(payloadPr1))
         syncPayload["comment"]["body"] = "argo sync " + appName
-        await probot.receive({name: "issue_comment", payload: syncPayload})
+        await probot.receive({ name: "issue_comment", payload: syncPayload })
     })
 })
